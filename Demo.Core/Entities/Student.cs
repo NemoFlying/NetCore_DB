4f3c7c150842ccf8f9d5575fee5ec89693fc5ccb
NemoFlying <895190626@qq.com>
2019-06-13 00:11:15 +0800

    Create-Project

---
+++ Demo.Core/Entities/Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Demo.Core.Entities
+{
+    [Table("STUDENT")]  //指定数据库对应表名
+    public class Student
+    {
+        /// <summary>
+        /// 学生学号
+        /// </summary>
+        [Key]  //主键
+        [Column("USERID")] //指定数据库对应表栏位名称
+        public string UserId { get; set; }
+
+        /// <summary>
+        /// 学生姓名
+        /// </summary>
+        [MaxLength(10)]
+        [Column("NAME")]
+        public string Name { get; set; }
+    }
+}
---
+++ Demo.EFCore/DemoDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Demo.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Demo.EFCore
+{
+    public class DemoDbContext : DbContext
+    {
+        public DemoDbContext(DbContextOptions<DemoDbContext> options)
+            :base(options)
+        {
+
+        }
+
+        //该处定义你要映射到数据库中的表
+        //格式固定
+        public DbSet<Student> Student { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //判断当前数据库是Oracle 需要手动添加Schema(DBA提供的数据库账号名称)
+            if(this.Database.IsOracle())
+            {
+                modelBuilder.HasDefaultSchema("NETCORE");
+            }
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
